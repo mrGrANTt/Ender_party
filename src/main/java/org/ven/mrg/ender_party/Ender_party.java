d4d2cd66caa9f3523df03826ed642ce03fc52420
mrGrANTt <sambistyyy0@gmail.com>
2025-03-08 19:08:56 +1000

    Values and base code added

---
+++ src/main/java/org/ven/mrg/ender_party/Ender_party.java
@@ -0,0 +1,17 @@
+package org.ven.mrg.ender_party;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ender_party extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/org/ven/mrg/ender_party/Values.java
@@ -0,0 +1,29 @@
+package org.ven.mrg.ender_party;
+
+import java.util.Random;
+
+public class Values {
+    private static Ender_party plg;
+    private static final Random rand = new Random();
+
+    public static void reload() {}
+
+    public static void setPlg(Ender_party value) { plg = value; }
+    public static Ender_party getPlg() { return plg; }
+
+    public static boolean isEnabled() { return plg.getConfig().getBoolean("enabled"); }
+
+    public static int getRandomSpawnTime() {
+        return rand.nextInt(
+            plg.getConfig().getInt("spawn-clown-timer.min"),
+            plg.getConfig().getInt("spawn-clown-timer.max") + 1
+        ) * 20;
+    }
+
+    public static int randomSpawnRadius() {
+        return rand.nextInt(1, plg.getConfig().getInt("spawn-radius"));
+    }
+
+    public static boolean hasClownAI() { return plg.getConfig().getBoolean("clown-ai"); }
+    public static boolean hasClownDead() { return plg.getConfig().getBoolean("clown-dead"); }
+}
