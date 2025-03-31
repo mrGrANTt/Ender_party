e048968ed81bf30fbd03820227422aa11d39558a
mrGrANTt <sambistyyy0@gmail.com>
2025-03-31 23:54:22 +1000

    Ender secret spawn

--- src/main/java/org/ven/mrg/ender_party/Values.java
+++ src/main/java/org/ven/mrg/ender_party/Values.java
@@ -16,8 +16,14 @@ public class Values {
     public static Ender_party getPlg() { return plg; }
 
     public static void logWithType(int type, String msg) {
-        if(type <= plg.getConfig().getInt("log-type", 1))
+        if(type >= plg.getConfig().getInt("log-type", 1))
             plg.getLogger().info(msg);
+    }
+
+    public static Random getRand() {return rand; }
+
+    public static TextColor getPlgColor() {
+        return TextColor.color(251, 177, 0);
     }
 
 
@@ -29,6 +35,8 @@ public class Values {
 
     public static boolean isEnabled() { return plg.getConfig().getBoolean("enabled"); }
 
+    public static int getSpawnRadius() { return plg.getConfig().getInt("spawn-radius"); }
+
     public static int getRandomSpawnTime() {
         return rand.nextInt(
             plg.getConfig().getInt("spawn-clown-timer.min"),
@@ -38,8 +46,4 @@ public class Values {
 
     public static boolean hasClownAI() { return plg.getConfig().getBoolean("clown-ai"); }
     public static boolean hasClownDead() { return plg.getConfig().getBoolean("clown-dead"); }
-
-    public static TextColor getPlgColor() {
-        return TextColor.color(251, 177, 0);
-    }
 }
--- src/main/java/org/ven/mrg/ender_party/custom/EnderController.java
+++ src/main/java/org/ven/mrg/ender_party/custom/EnderController.java
@@ -2,6 +2,7 @@ package org.ven.mrg.ender_party.custom;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Levelled;
@@ -17,6 +18,8 @@ import org.jetbrains.annotations.NotNull;
 import org.ven.mrg.ender_party.Values;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class EnderController extends MrgRunnable {
@@ -54,20 +57,68 @@ public class EnderController extends MrgRunnable {
     }
 
     public static Enderman getClown(Location loc, boolean teleport) {
+
+        if (teleport) {
+            Location location = getLocation(loc);
+            if (location != null) loc = location;
+        }
+
         Values.logWithType(0, "[EnderController] Spawning new e-man in " + loc.getBlockX() + " " + loc.getBlockY() + " " + loc.getBlockZ() + "!");
         Enderman eman = (Enderman) loc.getWorld().spawnEntity(loc, EntityType.ENDERMAN);
         eman.setMetadata("EnderClown", new FixedMetadataValue(Values.getPlg(), true));
 
         if (!Values.hasClownAI()) {
             eman.setAI(false);
-            new EnderNoAIGravity(eman).runTaskTimer(0,1);
+            //new EnderNoAIGravity(eman).runTaskTimer(0,1);
         }
 
-        if (teleport) teleport(10, eman);
-
+/*
         eman.getEquipment().setHelmet(getClownHead());
         eman.getEquipment().setDropChance(EquipmentSlot.HEAD, 1f);
+*/
         return eman;
+    }
+
+    private static Location getLocation(Location loc) {
+        class Loc {
+            private final int x;
+            private final int y;
+            private final int z;
+
+            public Loc(int x, int y, int z) {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+
+            public Location getLocation(World world) {
+                return new Location(world, x, y, z);
+            }
+        }
+
+        int blockX = loc.getBlockX();
+        int blockY = loc.getBlockY();
+        int blockZ = loc.getBlockZ();
+
+        List<Loc> locs = new ArrayList<>();
+        int r = Values.getSpawnRadius();
+        for(int x = -r; x <= r; x++)
+            for(int y = -r; y <= r; y++)
+                for(int z = -r; z <= r; z++)
+                    if (
+                           isValidBlock(loc.getWorld().getBlockAt(blockX + x, blockY + y, blockZ + z))
+                                   && isValidBlock(loc.getWorld().getBlockAt(blockX + x, blockY + y+1, blockZ + z))
+                                   && isValidBlock(loc.getWorld().getBlockAt(blockX + x, blockY + y+2, blockZ + z))
+                                   && !isValidBlock(loc.getWorld().getBlockAt(blockX + x, blockY + y-1, blockZ + z))
+                    ) locs.add(new Loc(x,y,z));
+
+        if(locs.isEmpty()) return null;
+        return locs.get(Values.getRand().nextInt(0, locs.size())).getLocation(loc.getWorld()).add(blockX,blockY,blockZ);
+    }
+
+    private static boolean isValidBlock(Block block) {
+        Values.logWithType(0, block.getType().toString());
+        return block.getType().toString().contains("AIR") || block.getType().equals(Material.STRUCTURE_VOID);
     }
 
     public static Enderman getClown(Location loc) {
@@ -133,7 +184,7 @@ public class EnderController extends MrgRunnable {
             case RUN_OUT:
                 if (!eman.isDead()) {
                     Values.logWithType(0, "[EnderController] E-man running out!");
-                    for(int i = 0; i < 10; i++) if (eman.teleportRandomly()) break;
+                    teleport(10);
                     phase = EnderPhase.HIDE;
                     new EnderController(phase, eman, block).runTaskLater(200);
                 }
